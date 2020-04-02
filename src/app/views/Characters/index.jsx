19a5d4f365e00a50e8036de43989e285a75d35cc
Gervis Bermudez <gbermudezmora@quaresitsolutions.com>
2020-04-01 21:45:55 -0300

    Remove unused imports

--- src/app/views/Characters/index.jsx
+++ src/app/views/Characters/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import Card from "../../components/Card";
 import Spinner from "../../components/Spinner";
